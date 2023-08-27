5b66182fbf4f0695157d3d783111c2dbd8cf018c
Luis Masuelli <luis@datagran.io>
2023-08-27 10:08:35 -0500

    Removing the reference to the Async extension methods workspace - this package now works on 2023.1

--- Runtime/Types/IPFSLoader.cs
+++ Runtime/Types/IPFSLoader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using AlephVault.Unity.Support.Types.Async;
 using UnityEngine;
 using UnityEngine.Networking;
 
